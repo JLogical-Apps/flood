fb9bbd12e708c4fcef8044af67bd7293e02f7ddb
Jake <jakeboychenko@yahoo.com>
2020-12-07 16:08:13 -0500

    Added smart image.

---
+++ lib/widgets/smart_image.dart
@@ -0,0 +1,24 @@
+import 'package:cache_image/cache_image.dart';
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+/// Image that handles caching, and fade in animation.
+class SmartImage extends StatelessWidget {
+  /// The url of the image.
+  final String url;
+
+  final double width;
+  final double height;
+
+  const SmartImage({@required this.url, this.width, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInImage(
+      placeholder: MemoryImage(kTransparentImage),
+      image: CacheImage(url),
+      width: width,
+      height: height,
+    );
+  }
+}
