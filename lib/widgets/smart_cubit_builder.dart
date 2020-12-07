b664a2b0e277d13a104d30ac790888c0cd2bd25e
Jake <jakeboychenko@yahoo.com>
2020-12-07 00:03:36 -0500

    Added smart_cubit_builder.

---
+++ lib/widgets/smart_cubit_builder.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jlogical_utils/widgets/loading_widget.dart';
+
+/// Builds a cubit in an easy way. L1 is a loading state. L2 is a loaded state.
+class SmartCubitBuilder<C extends Cubit<S>, S, L1 extends S, L2 extends S> extends StatelessWidget {
+  /// The cubit to build from. If omitted, will use cubit in the hierarchy tree.
+  final C cubit;
+
+  /// The widget to show when loading.
+  final Widget loadingWidget;
+
+  /// The widget to show after it is loaded.
+  final Widget Function(C cubit, L2 state) loadedWidget;
+
+  /// The widget to show during an error.
+  final Widget errorWidget;
+
+  const SmartCubitBuilder({
+    this.cubit,
+    @required this.loadedWidget,
+    this.loadingWidget: const LoadingWidget(),
+    this.errorWidget: const LoadingWidget(),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<C, S>(
+      cubit: cubit,
+      builder: (context, state) {
+        if (state is L1) {
+          return loadingWidget;
+        } else if (state is L2) {
+          return loadedWidget(cubit ?? context.bloc<C>(), state);
+        } else {
+          return errorWidget;
+        }
+      },
+    );
+  }
+}
