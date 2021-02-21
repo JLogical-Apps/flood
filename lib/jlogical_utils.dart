ec5133561172f29209755925de28520d1baf051c
Jake <jakeboychenko@yahoo.com>
2021-02-20 21:23:23 -0500

    Added model list.

--- lib/jlogical_utils.dart
+++ lib/jlogical_utils.dart
@@ -1,16 +1,14 @@
 export 'src/model/future_value.dart';
 export 'src/model/model.dart';
+export 'src/model/model_list.dart';
 export 'src/model/model_store.dart';
-
 export 'src/theme/theme.dart';
-
-export 'src/utils/popups/color_popup.dart';
-export 'src/utils/popups/input_popup.dart';
 export 'src/utils/format.dart';
 export 'src/utils/pair.dart';
 export 'src/utils/popup.dart';
+export 'src/utils/popups/color_popup.dart';
+export 'src/utils/popups/input_popup.dart';
 export 'src/utils/validators.dart';
-
 export 'src/widgets/category_card.dart';
 export 'src/widgets/clickable_card.dart';
 export 'src/widgets/duration_input_field.dart';
