eeaa8387b9de6280d00a8e7db32c08548e929379
Jake <jakeboychenko@yahoo.com>
2021-02-22 22:24:06 -0500

    Refactored ErrorText to now be able to be a button.

--- lib/src/widgets/error_text.dart
+++ lib/src/widgets/error_text.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:jlogical_utils/jlogical_utils.dart';
 
 /// Text that displays an error message and can retry a failed operation.
 class ErrorText extends StatelessWidget {
   /// The text of the error to display.
   final String errorText;
 
-  const ErrorText({@required this.errorText});
+  /// The error to show when clicked. If null, then this isn't clickable.
+  final dynamic error;
+
+  const ErrorText(this.errorText, {this.error});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      errorText,
-      style: TextStyle(color: Colors.red),
-    );
+    return error == null
+        ? Text(
+            errorText,
+            style: TextStyle(color: Colors.red),
+          )
+        : OutlinedButton(
+            child: Text(errorText),
+            onPressed: () {
+              Popup.message(context, title: 'Error Message', message: error.toString(), okText: 'OK');
+            },
+          );
   }
 }
