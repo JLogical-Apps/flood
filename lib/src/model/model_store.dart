fb06e9431f404a0f92997e5c8d3a8d90d248ba1b
Jake <jakeboychenko@yahoo.com>
2021-02-22 21:13:13 -0500

    Fixed bug in model store.

--- lib/src/model/model_store.dart
+++ lib/src/model/model_store.dart
@@ -3,7 +3,9 @@ import 'model.dart';
 /// A store for all the models of a particular type.
 class ModelStore<T> {
   /// Maps the model id to its model.
-  Map<String, Model<T>> _models;
+  final Map<String, Model<T>> _models;
+
+  ModelStore() : _models = Map();
 
   /// Returns the model of the entity with the given [id].
   /// If no model exists, it uses [onCreate] to generate a new one.
