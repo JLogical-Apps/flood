857ba61b07e8c6a60941e7ff5ff7396d823bd5f1
Jake <jakeboychenko@yahoo.com>
2021-02-22 21:58:27 -0500

    Replaced ModelList to use Map instead of a list of string-id Models.

--- lib/src/model/model_list.dart
+++ lib/src/model/model_list.dart
@@ -8,9 +8,9 @@ part 'model_list.g.dart';
 class ModelList<T> = ModelListBase<T> with _$ModelList<T>;
 
 abstract class ModelListBase<T> with Store {
-  /// The models in the list.
+  /// The ids and models in the list.
   @observable
-  FutureValue<List<Model<T>>> models;
+  FutureValue<Map<String, Model<T>>> modelsMap;
 
   /// A function that loads data to be stored in the model.
   FutureOr<Map<String, T>> Function() loader;
@@ -22,15 +22,15 @@ abstract class ModelListBase<T> with Store {
   Model<T> Function(String id, T entity) converter;
 
   /// Whether the model is in its initial state.
-  bool get isInitial => models is FutureValueInitial;
+  bool get isInitial => modelsMap is FutureValueInitial;
 
   /// Whether the model is loaded.
-  bool get isLoaded => models is FutureValueLoaded;
+  bool get isLoaded => modelsMap is FutureValueLoaded;
 
   /// Whether the model contains an error.
-  bool get isError => models is FutureValueError;
+  bool get isError => modelsMap is FutureValueError;
 
-  ModelListBase({this.loader, this.converter, List<Model<T>> initialModels}) : models = initialModels == null ? FutureValue.initial() : FutureValue.loaded(value: initialModels);
+  ModelListBase({this.loader, this.converter, Map<String, Model<T>> initialModels}) : modelsMap = initialModels == null ? FutureValue.initial() : FutureValue.loaded(value: initialModels);
 
   /// Loads the models using the loader.
   @action
@@ -42,11 +42,14 @@ abstract class ModelListBase<T> with Store {
 
     _completer = Completer();
 
-    if (models is FutureValueError) {
-      models = FutureValue.initial();
+    if (modelsMap is FutureValueError) {
+      modelsMap = FutureValue.initial();
     }
 
-    models = await FutureValue.guard(() async => (await loader()).entries.map((entry) => converter(entry.key, entry.value)).toList());
+    modelsMap = await FutureValue.guard(() async {
+      var _models = await loader();
+      return _models.map((id, entity) => MapEntry(id, converter(id, entity)));
+    });
 
     // Once the model completes loading, notify other [load] calls that the load has finished.
     _completer.complete();
@@ -54,23 +57,31 @@ abstract class ModelListBase<T> with Store {
   }
 
   /// Returns the loaded value of the model, or calls [orElse] if not loaded.
-  List<Model<T>> get({List<Model<T>> orElse()}) {
-    return models.maybeWhen(
-      loaded: (data) => data,
-      orElse: orElse,
-    );
-  }
+  /// Throws an exception if not loaded and [orElse] is null.
+  Map<String, Model<T>> get({Map<String, Model<T>> orElse()}) => modelsMap.maybeWhen(
+        loaded: (data) => data,
+        orElse: () => orElse != null ? orElse() : throw Exception('get() called without loaded state in ModelList!'),
+      );
+
+  /// Returns the ids of the loaded value of the model, or calls [orElse] if not loaded.
+  /// Throws an exception if not loaded and [orElse] is null.
+  List<String> getIDs({List<String> orElse()}) => get(orElse: () => null)?.keys?.toList() ?? orElse != null ? orElse() : throw Exception('getIDs() called without loaded state in ModelList!');
+
+  /// Returns the models of the loaded value of the model, or calls [orElse] if not loaded.
+  /// Throws an exception if not loaded and [orElse] is null.
+  List<Model<T>> getModels({List<Model<T>> orElse()}) =>
+      get(orElse: () => null)?.keys?.toList() ?? orElse != null ? orElse() : throw Exception('getModels() called without loaded state in ModelList!');
 
   /// Shorthand to getting the model's value.
-  List<Model<T>> call({List<Model<T>> orElse()}) => get(orElse: orElse);
+  Map<String, Model<T>> call({Map<String, Model<T>> orElse()}) => get(orElse: orElse);
 
   /// Waits for the model to finish loading and returns the loaded value of the model, or calls [onError] if an error occurred.
-  Future<List<Model<T>>> complete({List<Model<T>> onError(dynamic obj)}) async {
+  Future<Map<String, Model<T>>> complete({Map<String, Model<T>> onError(dynamic obj)}) async {
     if (isInitial) {
       await _completer.future;
     }
 
-    return models.when(
+    return modelsMap.when(
       initial: () => throw Exception('Model is in initial state after being loaded.'),
       loaded: (data) => data,
       error: (error) => onError(error),
--- lib/src/model/model_list.g.dart
+++ lib/src/model/model_list.g.dart
@@ -9,18 +9,18 @@ part of 'model_list.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ModelList<T> on ModelListBase<T>, Store {
-  final _$modelsAtom = Atom(name: 'ModelListBase.models');
+  final _$modelsMapAtom = Atom(name: 'ModelListBase.modelsMap');
 
   @override
-  FutureValue<List<Model<T>>> get models {
-    _$modelsAtom.reportRead();
-    return super.models;
+  FutureValue<Map<String, Model<T>>> get modelsMap {
+    _$modelsMapAtom.reportRead();
+    return super.modelsMap;
   }
 
   @override
-  set models(FutureValue<List<Model<T>>> value) {
-    _$modelsAtom.reportWrite(value, super.models, () {
-      super.models = value;
+  set modelsMap(FutureValue<Map<String, Model<T>>> value) {
+    _$modelsMapAtom.reportWrite(value, super.modelsMap, () {
+      super.modelsMap = value;
     });
   }
 
@@ -34,7 +34,7 @@ mixin _$ModelList<T> on ModelListBase<T>, Store {
   @override
   String toString() {
     return '''
-models: ${models}
+modelsMap: ${modelsMap}
     ''';
   }
 }
