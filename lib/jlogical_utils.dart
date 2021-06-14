b70c3516bb604666434ff5e7b7e40180e2f2ee12
Jake <jakeboychenko@yahoo.com>
2021-06-13 23:28:36 -0400

    Export pagination result.

--- lib/jlogical_utils.dart
+++ lib/jlogical_utils.dart
@@ -3,6 +3,7 @@ export 'src/model/model.dart';
 export 'src/model/model_list.dart';
 export 'src/model/model_store.dart';
 export 'src/model/paginated_model_list.dart';
+export 'src/model/pagination_result.dart';
 export 'src/theme/theme.dart';
 export 'src/utils/format.dart';
 export 'src/utils/pair.dart';
