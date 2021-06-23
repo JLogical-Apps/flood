2ba60893ef42e0630182239e900d70d4876b277b
Jake Boychenko <jakeboychenko@yahoo.com>
2021-06-23 18:20:26 -0400

    Added extensions to utils.

--- lib/jlogical_utils.dart
+++ lib/jlogical_utils.dart
@@ -15,6 +15,7 @@ export 'src/utils/pair.dart';
 export 'src/utils/popup.dart';
 export 'src/utils/popups/color_popup.dart';
 export 'src/utils/popups/input_popup.dart';
+export 'src/utils/stream_extensions.dart';
 export 'src/utils/utils.dart';
 export 'src/utils/validators.dart';
 export 'src/widgets/category_card.dart';
