3572b015681c062657cb5e0ec13a287e9623a01b
Jake Boychenko <jakeboychenko@yahoo.com>
2021-06-23 21:11:33 -0400

    Fixed ValueStream.mapWithValue

--- lib/src/utils/stream_extensions.dart
+++ lib/src/utils/stream_extensions.dart
@@ -3,6 +3,6 @@ import 'package:rxdart/rxdart.dart';
 extension ValueStreamExtensions<T> on ValueStream<T>{
   /// Maps this value stream to another value stream that contains the last mapped value.
   ValueStream<S> mapWithValue<S>(S convert(T value)){
-    return map(convert).shareValueSeeded(convert(value));
+    return map(convert).publishValueSeeded(convert(value)).autoConnect();
   }
 }
