6ad283f5d1214ca24f6da7164ef9f6389f518525
Jake <jakeboychenko@yahoo.com>
2021-08-12 16:28:29 -0400

    Added some string extensions.

--- lib/src/utils/string_extensions.dart
+++ lib/src/utils/string_extensions.dart
@@ -3,5 +3,27 @@ extension StringExtensions on String {
   bool get isBlank => trim().isEmpty;
 
   /// Whether the text represents an email.
-  bool get isEmail => RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$").allMatches(this).isNotEmpty;
+  bool get isEmail => RegExp(
+          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+      .allMatches(this)
+      .isNotEmpty;
+
+  /// Returns everything after the first instance of [pattern]. If this doesn't contain [pattern], then returns a blank string.
+  String allAfter(Pattern pattern) {
+    final index = indexOf(pattern);
+    if (index == -1) {
+      return '';
+    }
+    return substring(index);
+  }
+
+  /// Returns everything before the first instance of [pattern]. If this doesn't contain [pattern], then returns a blank string.
+  String allBefore(Pattern pattern) {
+    final index = indexOf(pattern);
+    if (index == -1) {
+      return '';
+    }
+
+    return substring(0, index);
+  }
 }
