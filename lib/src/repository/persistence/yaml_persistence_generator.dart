d0c10452197275e830ee5bb63c0cabe3bc630038
Jake <jakeboychenko@yahoo.com>
2021-08-14 13:27:24 -0400

    Added Yaml Persistence Generator base.

---
+++ lib/src/repository/persistence/yaml_persistence_generator.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:jlogical_utils/src/repository/persistence/persistence_generator.dart';
+import 'package:json2yaml/json2yaml.dart';
+import 'package:yaml/yaml.dart';
+
+/// A persistence generator that generates yaml. Currently does not support saving to yaml, only reading from yaml.
+abstract class YamlPersistenceGenerator<T> implements PersistenceGenerator<T, String> {
+  /// Maps an object to json, which will be converted to yaml when saving.
+  Map<String, dynamic> toJson(T object);
+
+  /// Maps the yaml's json to an object.
+  T fromJson(Map<String, dynamic> jsonObject);
+
+  @override
+  String save(T object) {
+    return json2yaml(toJson(object));
+  }
+
+  @override
+  T load(String storage) {
+    return fromJson(json.decode(json.encode(loadYaml(storage))));
+  }
+}
---
+++ test/test.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jlogical_utils/src/repository/persistence/yaml_persistence_generator.dart';
+
+void main() {
+  test('yaml', () {
+    final yaml = ConfigYamlPersistenceGenerator();
+    final config = Config(version: 3);
+    expect(yaml.save(config), 'version: 3');
+    expect(yaml.load('version: 3'), config);
+  });
+}
+
+class Config extends Equatable {
+  final int version;
+
+  const Config({required this.version});
+
+  @override
+  List<Object?> get props => [version];
+}
+
+class ConfigYamlPersistenceGenerator extends YamlPersistenceGenerator<Config> {
+  @override
+  Config fromJson(Map<String, dynamic> jsonObject) {
+    return Config(version: jsonObject['version']);
+  }
+
+  @override
+  Map<String, dynamic> toJson(Config config) => {
+        'version': config.version,
+      };
+}
