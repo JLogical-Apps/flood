f2348160c3cf97d88949609a276f603c1bf47b5f
Jake <jakeboychenko@yahoo.com>
2021-09-06 17:38:10 -0400

    Refactor and add new StyledDialogs.

--- lib/src/style/widgets/pages/styled_dialog.dart
+++ lib/src/style/widgets/pages/styled_dialog.dart
@@ -28,12 +28,42 @@ class StyledDialog<T> {
     return result;
   }
 
+  static StyledDialog<bool> yesNo({
+    required BuildContext context,
+    String? titleText,
+    Widget? title,
+    required List<Widget> children,
+    String confirmText: 'Ok',
+  }) {
+    final style = context.style();
+    final smartFormController = SmartFormController();
+
+    return StyledDialog(
+      titleText: titleText,
+      title: title,
+      body: SmartForm(
+        controller: smartFormController,
+        child: Column(
+          children: [
+            ...children,
+            StyledButton.high(
+              text: confirmText,
+              onTapped: () {
+                style.navigateBack(context: context, result: true);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   /// Wraps a styled dialog around a smart form and returns the data from the smart form.
   static StyledDialog<Map<String, dynamic>?> smartForm({
     required BuildContext context,
     String? titleText,
     Widget? title,
-    required Widget body,
+    required List<Widget> children,
     Widget Function(SmartFormController controller)? confirmButtonBuilder,
   }) {
     final style = context.style();
@@ -59,7 +89,7 @@ class StyledDialog<T> {
         controller: smartFormController,
         child: Column(
           children: [
-            body,
+            ...children,
             confirmButton,
           ],
         ),
