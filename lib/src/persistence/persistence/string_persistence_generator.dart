891468ae15d3fbad4c31ee668bd3d6d95f9169f6
Jake <jakeboychenko@yahoo.com>
2021-09-26 00:47:20 -0400

    Add StringPersistenceGenerator and StringFileDataSource

---
+++ lib/src/persistence/persistence/string_persistence_generator.dart
@@ -0,0 +1,20 @@
+import '../persistence.dart';
+
+/// A persistence generator that generates json.
+abstract class StringPersistenceGenerator<T> implements PersistenceGenerator<T, String> {
+  /// Maps an object to a string.
+  String toPersistedString(T object);
+
+  /// Maps a string to an object.
+  T fromPersistedString(String string);
+
+  @override
+  String save(T object) {
+    return toPersistedString(object);
+  }
+
+  @override
+  T load(String storage) {
+    return fromPersistedString(storage);
+  }
+}
