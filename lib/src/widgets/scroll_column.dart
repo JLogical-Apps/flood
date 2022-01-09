c82b63ecc0cf55bcb4e4c9b416f4c60c359aaee2
Jake <jakeboychenko@yahoo.com>
2022-01-08 20:10:54 -0500

    ScrollColumn can be refreshed

--- lib/src/widgets/scroll_column.dart
+++ lib/src/widgets/scroll_column.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 /// A utility widget of a Column wrapped in a SingleChildScrollView with an optional Scrollbar.
-class ScrollColumn extends StatelessWidget {
+class ScrollColumn extends HookWidget {
   /// Whether to show a Scrollbar.
   final bool withScrollbar;
 
   /// The children of the column.
   final List<Widget> children;
 
-  const ScrollColumn({Key? key, this.withScrollbar: false, required this.children}) : super(key: key);
+  final Future Function()? onRefresh;
 
-  const ScrollColumn.withScrollbar({Key? key, required List<Widget> children})
-      : this(key: key, withScrollbar: true, children: children);
+  const ScrollColumn({Key? key, this.withScrollbar: false, required this.children, this.onRefresh}) : super(key: key);
+
+  const ScrollColumn.withScrollbar({Key? key, required List<Widget> children, Future Function()? onRefresh})
+      : this(key: key, withScrollbar: true, children: children, onRefresh: onRefresh);
 
   @override
   Widget build(BuildContext context) {
-    var scrollView = SingleChildScrollView(
+    Widget scrollView = SingleChildScrollView(
       child: Column(children: children),
       physics: BouncingScrollPhysics(),
     );
+    if (onRefresh != null) {
+      final refreshController = useMemoized(() => RefreshController());
+      scrollView = SmartRefresher(
+        controller: refreshController,
+        onRefresh: () async {
+          await onRefresh!();
+          refreshController.refreshCompleted();
+        },
+        child: scrollView,
+      );
+    }
     return withScrollbar ? Scrollbar(child: scrollView) : scrollView;
   }
 }
