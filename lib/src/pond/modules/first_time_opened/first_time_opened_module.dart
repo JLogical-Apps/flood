2a4d664b017d6aaa438b763cfa1616d853042672
Jake <jakeboychenko@yahoo.com>
2022-01-17 23:29:47 -0500

    Fix FirstTimeOpenedModule

--- lib/src/pond/modules/first_time_opened/first_time_opened_module.dart
+++ lib/src/pond/modules/first_time_opened/first_time_opened_module.dart
@@ -1,11 +1,22 @@
+import 'dart:io';
+
 import 'package:jlogical_utils/jlogical_utils.dart';
-import 'package:jlogical_utils/src/pond/context/module/app_module.dart';
+import 'package:jlogical_utils/src/pond/modules/first_time_opened/app_opened_marker_repository.dart';
 
 import 'app_opened_marker.dart';
 import 'app_opened_marker_entity.dart';
 
 class FirstTimeOpenedModule extends AppModule {
+  final Directory baseDirectory;
+
   bool? isFirstTimeOpened;
+
+  FirstTimeOpenedModule({required this.baseDirectory});
+
+  @override
+  void onRegister(AppRegistration registration) {
+    registration.register(AppOpenedMarkerRepository(baseDirectory: baseDirectory / 'meta'));
+  }
 
   Future<void> onLoad() async {
     final existingAppOpenedMarker = await Query.from<AppOpenedMarkerEntity>().firstOrNull().get();
