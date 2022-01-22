b534d69a1598eae9ee0a30743085dd9e4fb5f622
Jake <jakeboychenko@yahoo.com>
2022-01-21 19:43:12 -0500

    Add initial data to LocalDataSource

--- lib/src/persistence/data_source/local_data_source.dart
+++ lib/src/persistence/data_source/local_data_source.dart
@@ -2,4 +2,8 @@ import 'package:jlogical_utils/src/persistence/data_source/data_source.dart';
 import 'package:jlogical_utils/src/persistence/data_source/with_local_data_source.dart';
 
 /// Data source that stores its data locally.
-class LocalDataSource<T> extends DataSource<T> with WithLocalDataSource<T> {}
+class LocalDataSource<T> extends DataSource<T> with WithLocalDataSource<T> {
+  final T? initialData;
+
+  LocalDataSource({this.initialData});
+}
