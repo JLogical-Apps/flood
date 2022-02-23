bf012634b22cb604a0d17e43cd337ac9c9bc74a8
Jake <jakeboychenko@yahoo.com>
2022-02-23 16:22:25 -0500

    Remove unecessary classes

--- lib/src/pond/export.dart
+++ lib/src/pond/export.dart
@@ -71,7 +71,5 @@ export 'type_state_serializers/type_state_serializer.dart';
 export 'utils/pond_hooks.dart';
 export 'utils/resolvable.dart';
 export 'utils/splash_page.dart';
-export 'utils/synchronizable.dart';
-export 'utils/with_key_synchronizable.dart';
 export 'validation/validation_exception.dart';
 export 'validation/validator.dart';
--- lib/src/pond/utils/synchronizable.dart
+++
@@ -1,5 +0,0 @@
-abstract class Synchronizable<L> {
-  Future<void> lock(L lock);
-
-  void unlock(L lock);
-}
