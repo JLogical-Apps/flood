99ea21f1ae367e6cb90edb94afe980b8d4ba2b05
jaboyc <jakeboychenko@yahoo.com>
2022-04-12 22:25:40 -0400

    Can have purely empty directory bundle

--- lib/src/pond/context/directory/directory_bundle.dart
+++ lib/src/pond/context/directory/directory_bundle.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
 
 import 'package:jlogical_utils/src/pond/context/directory/directory_provider.dart';
-import 'package:jlogical_utils/src/utils/file_extensions.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DirectoryBundle implements DirectoryProvider {
   @override
-  final Directory supportDirectory;
+  Directory get supportDirectory => _supportDirectory!;
 
-  const DirectoryBundle({required this.supportDirectory});
+  final Directory? _supportDirectory;
+
+  DirectoryBundle({Directory? supportDirectory}) : _supportDirectory = supportDirectory;
 
   static DirectoryBundle empty() {
-    return DirectoryBundle(supportDirectory: Directory.current / '_junk');
+    return DirectoryBundle();
   }
 
   static Future<DirectoryBundle> generate() async {
