69fbb1fe0a99669e3af5abd9352a26bb52312277
Jake <jakeboychenko@yahoo.com>
2022-04-16 19:18:10 -0400

    Standardize automation output

--- lib/src/pond/automation/automation_module.dart
+++ lib/src/pond/automation/automation_module.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:jlogical_utils/src/pond/automation/automation.dart';
 import 'package:jlogical_utils/src/pond/automation/automations_provider.dart';
+import 'package:jlogical_utils/src/utils/file_extensions.dart';
+
+import 'automate.dart';
 
 abstract class AutomationModule implements AutomationsProvider {
   String get name;
@@ -7,4 +12,6 @@ abstract class AutomationModule implements AutomationsProvider {
   final List<Automation> automations = [];
 
   String get categoryName => name;
+
+  Directory get cacheDirectory => automateOutputDirectory / name;
 }
