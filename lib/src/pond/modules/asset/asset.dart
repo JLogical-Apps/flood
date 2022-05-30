384a79c281ed49af0e0e90d94dea0c8724da29ea
Jake <jakeboychenko@yahoo.com>
2022-05-29 20:44:13 -0400

    Assets can be cached.

--- lib/src/pond/modules/asset/asset.dart
+++ lib/src/pond/modules/asset/asset.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'dart:typed_data';
+
+import 'package:jlogical_utils/src/pond/export.dart';
+import 'package:jlogical_utils/src/utils/export_core.dart';
 
 class Asset {
   final String? id;
@@ -6,4 +10,10 @@ class Asset {
   final Uint8List value;
 
   const Asset({this.id, required this.name, required this.value});
+
+  Future<File> cacheToFile() async {
+    final file = AppContext.global.cacheDirectory - name;
+    await file.writeAsBytes(value);
+    return file;
+  }
 }
