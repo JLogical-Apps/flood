0f68156e4d7a4e62163be95fe3e07ac4d4dbdbc6
jaboyc <jakeboychenko@yahoo.com>
2022-07-26 18:00:21 -0400

    Add StyledRequiredIndicator.

--- lib/src/port/export.dart
+++ lib/src/port/export.dart
@@ -4,4 +4,5 @@ export 'ui/port_builder.dart';
 export 'ui/port_exception_field_widget.dart';
 export 'ui/port_field_widget.dart';
 export 'ui/port_update_builder.dart';
+export 'ui/styled_required_indicator.dart';
 export 'util/with_port_exception_text_getter.dart';
--- lib/src/style/widgets/port/with_required_label_getter.dart
+++ lib/src/style/widgets/port/with_required_label_getter.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:jlogical_utils/src/port/export_core.dart';
-import 'package:jlogical_utils/src/style/style.dart';
+import 'package:jlogical_utils/src/port/ui/styled_required_indicator.dart';
 import 'package:jlogical_utils/src/utils/export_core.dart';
 
 import '../../../port/ui/port_field_widget.dart';
 import '../text/styled_body_text.dart';
-import '../text/styled_text_overrides.dart';
 
 mixin WithRequiredLabelGetter<F extends PortField<T>, T, R> on PortFieldWidget<F, T, R> {
   String? get labelText;
@@ -27,20 +26,7 @@ mixin WithRequiredLabelGetter<F extends PortField<T>, T, R> on PortFieldWidget<F
 
     final shouldShowRequiredIndicator = showRequiredIndicator ?? field.isRequired();
     if (shouldShowRequiredIndicator) {
-      widgetLabel = Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          widgetLabel,
-          StyledBodyText(
-            '*',
-            textOverrides: StyledTextOverrides(
-              fontWeight: FontWeight.bold,
-              fontColor: context.styleContext().emphasisColor,
-              padding: EdgeInsets.zero,
-            ),
-          ),
-        ],
-      );
+      widgetLabel = StyledRequiredIndicator(child: widgetLabel);
     }
 
     return widgetLabel;
