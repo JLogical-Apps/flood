02ff96807a2bc1e877e3117bb93738a57501338e
jaboyc <jakeboychenko@yahoo.com>
2022-10-25 20:29:02 -0400

    Add file pool.

--- lib/src/persistence/data_source/with_file_data_source.dart
+++ lib/src/persistence/data_source/with_file_data_source.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:jlogical_utils/src/persistence/data_source/data_source.dart';
 import 'package:jlogical_utils/src/utils/export_core.dart';
+import 'package:pool/pool.dart';
 
 mixin WithFileDataSource on DataSource<String> {
+  static Pool _filePool = Pool(10);
+
   /// The file this is pointing to.
   File get file;
 
@@ -15,12 +18,14 @@ mixin WithFileDataSource on DataSource<String> {
   Future<String?> getData() async {
     if (!await file.exists()) return null;
 
-    return await file.readAsString();
+    return await _filePool.withResource(() => file.readAsString());
   }
 
   Future<void> saveData(String persisted) async {
-    await file.ensureCreated();
-    await file.writeAsString(persisted);
+    await _filePool.withResource(() async {
+      await file.ensureCreated();
+      await file.writeAsString(persisted);
+    });
   }
 
   Future<void> delete() async {
--- lib/src/persistence/data_source/with_raw_file_data_source.dart
+++ lib/src/persistence/data_source/with_raw_file_data_source.dart
@@ -3,8 +3,11 @@ import 'dart:typed_data';
 
 import 'package:jlogical_utils/src/persistence/data_source/data_source.dart';
 import 'package:jlogical_utils/src/utils/export_core.dart';
+import 'package:pool/pool.dart';
 
 mixin WithRawFileDataSource on DataSource<Uint8List> {
+  static Pool _filePool = Pool(10);
+
   /// The file this is pointing to.
   File get file;
 
@@ -16,12 +19,14 @@ mixin WithRawFileDataSource on DataSource<Uint8List> {
   Future<Uint8List?> getData() async {
     if (!await file.exists()) return null;
 
-    return await guardAsync(() => file.readAsBytes());
+    return await _filePool.withResource(() => guardAsync(() => file.readAsBytes()));
   }
 
   Future<void> saveData(Uint8List data) async {
-    await file.ensureCreated();
-    await file.writeAsBytes(data);
+    await _filePool.withResource(() async {
+      await file.ensureCreated();
+      await file.writeAsBytes(data);
+    });
   }
 
   Future<void> delete() async {
