4ba963f1b8b61a1aefe0978d99387b416258a646
jaboyc <jakeboychenko@yahoo.com>
2022-12-29 15:52:44 -0500

    Fix Scrollbar issues.

--- packages/style/lib/src/flat/layout/flat_style_list.dart
+++ packages/style/lib/src/flat/layout/flat_style_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intersperse/intersperse.dart';
 import 'package:style/src/components/layout/styled_list.dart';
+import 'package:style/src/style_build_context_extensions.dart';
 import 'package:style/src/style_renderer.dart';
 
 class FlatStyleListRenderer with IsTypedStyleRenderer<StyledList> {
@@ -11,14 +12,29 @@ class FlatStyleListRenderer with IsTypedStyleRenderer<StyledList> {
     Widget widget = component.axis == Axis.vertical ? Column(children: children) : Row(children: children);
 
     if (component.isScrollable) {
-      widget = SingleChildScrollView(child: widget, scrollDirection: component.axis);
+      final scrollController = ScrollController();
+
+      widget = SingleChildScrollView(
+        child: widget,
+        controller: scrollController,
+        scrollDirection: component.axis,
+        physics: BouncingScrollPhysics(),
+      );
 
       if (component.hasScrollbar) {
         widget = MediaQuery.removePadding(
           context: context,
           removeTop: true,
           removeBottom: true,
-          child: Scrollbar(child: widget),
+          child: RawScrollbar(
+            child: widget,
+            controller: scrollController,
+            thumbColor: context.colorPalette().foreground.regular.withOpacity(0.6),
+            trackColor: context.colorPalette().background.subtle,
+            // trackBorderColor: Colors.red,
+            radius: Radius.circular(12),
+            trackRadius: Radius.circular(12),
+          ),
         );
       }
     }
