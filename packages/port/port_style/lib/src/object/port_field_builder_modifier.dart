ff3c27980006ec42fe0f314bb4e08d1e9c55101f
jaboyc <jakeboychenko@yahoo.com>
2023-03-26 13:30:04 -0400

    Remove interface port field builder.

--- packages/port/port_style/lib/src/object/port_field_builder_modifier.dart
+++ packages/port/port_style/lib/src/object/port_field_builder_modifier.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:port/port.dart';
 import 'package:port_style/src/object/currency_port_field_builder_modifier.dart';
-import 'package:port_style/src/object/interface_port_field_builder_modifier.dart';
 import 'package:port_style/src/object/string_port_field_builder_modifier.dart';
 import 'package:utils/utils.dart';
 
@@ -10,7 +9,6 @@ abstract class PortFieldBuilderModifier with IsModifier<PortField> {
 
   static final portFieldBuilderModifierResolver = ModifierResolver<PortFieldBuilderModifier, PortField>(modifiers: [
     StringPortFieldBuilderModifier(),
-    InterfacePortFieldBuilderModifier(),
     CurrencyPortFieldBuilderModifier(),
   ]);
 
