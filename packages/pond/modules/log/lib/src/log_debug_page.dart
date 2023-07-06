58916028205d82c46028b3e9c03d550d2fa808ed
jaboyc <jakeboychenko@yahoo.com>
2023-07-06 15:01:44 -0400

    Share logs from debug log page.

--- packages/pond/modules/log/lib/src/log_debug_page.dart
+++ packages/pond/modules/log/lib/src/log_debug_page.dart
@@ -3,7 +3,9 @@ import 'package:intersperse/intersperse.dart';
 import 'package:log/log.dart';
 import 'package:model/model.dart';
 import 'package:pond/pond.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:style/style.dart';
+import 'package:utils/utils.dart';
 
 class LogDebugPage extends AppPage {
   @override
@@ -12,8 +14,26 @@ class LogDebugPage extends AppPage {
   @override
   Widget build(BuildContext context) {
     final logsModel = useFutureModel(() => context.getLogs());
+    final logs = logsModel.getOrNull();
+
     return StyledPage(
       titleText: 'Logs',
+      actions: [
+        if (logs != null)
+          ActionItem(
+              titleText: 'Share',
+              descriptionText: 'Share the logs with a developer.',
+              iconData: Icons.share,
+              color: Colors.blue,
+              onPerform: (context) async {
+                final box = context.findRenderObject() as RenderBox?;
+                await Share.share(
+                  logs.join('\n'),
+                  subject: 'Logs ${DateTime.now().format()}',
+                  sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
+                );
+              }),
+      ],
       body: ModelBuilder(
         model: logsModel,
         builder: (List<String> logs) {
