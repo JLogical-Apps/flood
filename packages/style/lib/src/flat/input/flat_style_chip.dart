00794d6a2c20ad45a5caeec2bd19fd99af2b64d8
jaboyc <jakeboychenko@yahoo.com>
2023-08-12 16:35:50 -0400

    Fix StyledChip onPressed not working.

--- packages/style/lib/src/flat/input/flat_style_chip.dart
+++ packages/style/lib/src/flat/input/flat_style_chip.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:style/src/color_palette_provider.dart';
 import 'package:style/src/components/input/styled_chip.dart';
 import 'package:style/src/components/layout/styled_container.dart';
 import 'package:style/src/components/layout/styled_list.dart';
 import 'package:style/src/components/misc/styled_icon.dart';
+import 'package:style/src/components/misc/styled_loading_indicator.dart';
 import 'package:style/src/components/text/styled_text.dart';
 import 'package:style/src/style_build_context_extensions.dart';
 import 'package:style/src/style_renderer.dart';
@@ -19,8 +21,22 @@ class FlatStyleChipRenderer with IsTypedStyleRenderer<StyledChip> {
         component.backgroundColor?.mapIfNonNull((color) => context.style().getColorPaletteFromBackground(color)) ??
             context.colorPalette().background.getByEmphasis(component.emphasis);
 
+    final loadingState = useState<bool>(false);
+
+    final onPressed = component.onPressed?.mapIfNonNull(
+      (onPressed) => () async {
+        if (loadingState.value) {
+          return;
+        }
+
+        loadingState.value = true;
+        await onPressed();
+        loadingState.value = false;
+      },
+    );
+
     return IntrinsicWidth(
-      child: Chip(
+      child: ActionChip(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(100),
           side: BorderSide(
@@ -30,14 +46,30 @@ class FlatStyleChipRenderer with IsTypedStyleRenderer<StyledChip> {
         ),
         label: ColorPaletteProvider(
           colorPalette: backgroundColorPalette,
-          child: StyledList.row(
+          child: Stack(
             children: [
-              if (icon != null) icon,
-              if (label != null) label,
+              Opacity(
+                opacity: loadingState.value ? 0 : 1,
+                child: StyledList.row(
+                  children: [
+                    if (icon != null) icon,
+                    if (label != null) label,
+                  ],
+                ),
+              ),
+              Positioned.fill(
+                child: Center(
+                  child: Visibility(
+                    visible: loadingState.value,
+                    child: StyledLoadingIndicator(),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
         backgroundColor: backgroundColorPalette,
+        onPressed: onPressed,
       ),
     );
   }
