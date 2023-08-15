f24a84c550c976b24cb5025d0ec9388fe5993d9d
jaboyc <jakeboychenko@yahoo.com>
2023-08-15 14:13:59 -0400

    Write Permission.unmodifiable() to firestore rules.

---
+++ packages/pond/modules/ops/lib/src/permission/and_permission_text_modifier.dart
@@ -0,0 +1,15 @@
+import 'package:drop_core/drop_core.dart';
+import 'package:ops/src/permission/permission_text_modifier.dart';
+
+class AndPermissionTextModifier extends PermissionTextModifier<AndPermission> {
+  final PermissionTextModifier Function(Permission permission) permissionTextModifierGetter;
+
+  AndPermissionTextModifier({required this.permissionTextModifierGetter});
+
+  @override
+  String getText(DropCoreContext context, AndPermission permission) {
+    return permission.permissions
+        .map((permission) => '(${permissionTextModifierGetter(permission).getText(context, permission)})')
+        .join(' && ');
+  }
+}
