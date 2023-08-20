311649404511b89fb3bd7ba772d5992285ec6b8a
jaboyc <jakeboychenko@yahoo.com>
2023-08-20 14:13:04 -0400

    Export FirebaseCloudRepositoryStateHandler

--- packages/pond/modules/firebase/lib/firebase.dart
+++ packages/pond/modules/firebase/lib/firebase.dart
@@ -1,3 +1,4 @@
 export 'src/auth/firebase_auth_service_implementation.dart';
 export 'src/drop/firebase_cloud_repository_implementation.dart';
+export 'src/drop/firebase_cloud_repository_state_handler.dart';
 export 'src/firebase_core_component.dart';
