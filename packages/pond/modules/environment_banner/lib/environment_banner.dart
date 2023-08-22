3123635fadfc96ebb937d6a22218acc345d8fdbb
jaboyc <jakeboychenko@yahoo.com>
2023-08-22 17:55:56 -0400

    Allow hiding Environment Banner in production.

--- packages/pond/modules/environment_banner/lib/environment_banner.dart
+++ packages/pond/modules/environment_banner/lib/environment_banner.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:pond/pond.dart';
 
 class EnvironmentBannerAppComponent with IsAppPondComponent {
+  final bool Function(EnvironmentType environmentType)? shouldShow;
+
+  EnvironmentBannerAppComponent({this.shouldShow});
+
   @override
   Widget wrapPage(AppPondContext context, Widget page, AppPondPageContext pageContext) {
+    final shouldShow = this.shouldShow ?? (environment) => environment != EnvironmentType.static.production;
+    if (!shouldShow(context.environment)) {
+      return page;
+    }
+
     return Banner(
       message: context.environment.name.toUpperCase(),
       location: BannerLocation.topEnd,
