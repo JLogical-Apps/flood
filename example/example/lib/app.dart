d22c3cb635d705e1882ea38f7831fef7e8c9f984
jaboyc <jakeboychenko@yahoo.com>
2023-09-11 20:25:13 -0400

    Refactor testing to another file.

--- example/example/lib/app.dart
+++ example/example/lib/app.dart
@@ -8,22 +8,7 @@ import 'package:example/presentation/port/repeating_goal_port_override.dart';
 import 'package:example/presentation/port/transfer_transaction_port_override.dart';
 import 'package:example/presentation/style.dart';
 import 'package:example/presentation/valet_pages_pond_component.dart';
-import 'package:example_core/features/budget/budget.dart';
-import 'package:example_core/features/budget/budget_entity.dart';
-import 'package:example_core/features/envelope/envelope.dart';
-import 'package:example_core/features/envelope/envelope_entity.dart';
-import 'package:example_core/features/envelope_rule/daily_time_rule.dart';
-import 'package:example_core/features/envelope_rule/firstfruit_envelope_rule.dart';
-import 'package:example_core/features/envelope_rule/monthly_time_rule.dart';
-import 'package:example_core/features/envelope_rule/repeating_goal_envelope_rule.dart';
-import 'package:example_core/features/envelope_rule/surplus_envelope_rule.dart';
-import 'package:example_core/features/envelope_rule/target_goal_envelope_rule.dart';
-import 'package:example_core/features/transaction/budget_transaction_entity.dart';
-import 'package:example_core/features/transaction/envelope_transaction.dart';
-import 'package:example_core/features/transaction/income_transaction.dart';
-import 'package:example_core/features/transaction/transfer_transaction.dart';
-import 'package:example_core/features/tray/tray.dart';
-import 'package:example_core/features/tray/tray_entity.dart';
+import 'package:example/testing.dart';
 import 'package:example_core/features/user/user.dart';
 import 'package:example_core/features/user/user_entity.dart';
 import 'package:example_core/pond.dart';
@@ -127,128 +112,10 @@ Future<AppPondContext> getAppPondContext(CorePondContext corePondContext) async 
   await appPondContext.register(EnvironmentBannerAppComponent());
   await appPondContext.register(TestingSetupAppComponent(onSetup: () async {
     if (testingLoggedIn) {
-      await _setupTesting(corePondContext);
+      await setupTesting(corePondContext);
     }
   }));
   await appPondContext.register(ValetPagesAppPondComponent());
 
   return appPondContext;
-}
-
-Future<void> _setupTesting(CorePondContext corePondContext) async {
-  final authComponent = corePondContext.locate<AuthCoreComponent>();
-  final dropComponent = corePondContext.locate<DropCoreComponent>();
-
-  final userId = await authComponent.signup('test@test.com', 'password');
-
-  final userEntity = await dropComponent.updateEntity(
-    UserEntity()..id = userId,
-    (User user) => user.nameProperty.set('John Doe'),
-  );
-
-  final budgetEntity = await dropComponent.updateEntity(
-    BudgetEntity(),
-    (Budget budget) => budget
-      ..nameProperty.set('Budget')
-      ..ownerProperty.set(userEntity.id!),
-  );
-
-  final trayEntity = await corePondContext.dropCoreComponent.updateEntity(
-      TrayEntity(),
-      (Tray tray) => tray
-        ..nameProperty.set('Repeating')
-        ..budgetProperty.set(budgetEntity.id!));
-
-  final envelopes = [
-    Envelope()
-      ..budgetProperty.set(budgetEntity.id!)
-      ..nameProperty.set('Tithe')
-      ..ruleProperty.set(FirstfruitEnvelopeRule()..percentProperty.set(10)),
-    Envelope()
-      ..budgetProperty.set(budgetEntity.id!)
-      ..nameProperty.set('Car')
-      ..trayProperty.set(trayEntity.id!)
-      ..ruleProperty.set(RepeatingGoalEnvelopeRule()
-        ..goalCentsProperty.set(100 * 100)
-        ..timeRuleProperty.set(MonthlyTimeRule())
-        ..remainingGoalCentsProperty.set(100 * 100)
-        ..lastAppliedDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 6))))),
-    Envelope()
-      ..budgetProperty.set(budgetEntity.id!)
-      ..nameProperty.set('Eating Out')
-      ..trayProperty.set(trayEntity.id!)
-      ..ruleProperty.set(RepeatingGoalEnvelopeRule()
-        ..goalCentsProperty.set(40 * 100)
-        ..timeRuleProperty.set(DailyTimeRule()..daysProperty.set(7))
-        ..remainingGoalCentsProperty.set(40 * 100)
-        ..lastAppliedDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 4))))),
-    Envelope()
-      ..budgetProperty.set(budgetEntity.id!)
-      ..nameProperty.set('Emergency Savings')
-      ..ruleProperty.set(TargetGoalEnvelopeRule()
-        ..percentProperty.set(20)
-        ..maximumCentsProperty.set(1000 * 100)),
-    Envelope()
-      ..budgetProperty.set(budgetEntity.id!)
-      ..nameProperty.set('Savings')
-      ..ruleProperty.set(SurplusEnvelopeRule()..percentProperty.set(100)),
-  ];
-
-  await Future.wait(envelopes
-      .map((envelope) => EnvelopeEntity()
-        ..id = envelope.nameProperty.value
-        ..set(envelope))
-      .map((entity) => dropComponent.update(entity)));
-
-  final transactions = [
-    EnvelopeTransaction()
-      ..nameProperty.set('Payment')
-      ..amountCentsProperty.set(-90 * 100)
-      ..envelopeProperty.set('Car')
-      ..budgetProperty.set(budgetEntity.id!)
-      ..transactionDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 3)))),
-    EnvelopeTransaction()
-      ..nameProperty.set('Vacation')
-      ..amountCentsProperty.set(-180 * 100)
-      ..envelopeProperty.set('Savings')
-      ..budgetProperty.set(budgetEntity.id!)
-      ..transactionDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 3)))),
-    EnvelopeTransaction()
-      ..nameProperty.set('Plumber')
-      ..amountCentsProperty.set(-120 * 100)
-      ..envelopeProperty.set('Emergency Savings')
-      ..budgetProperty.set(budgetEntity.id!)
-      ..transactionDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 3)))),
-    TransferTransaction()
-      ..amountCentsProperty.set(20 * 100)
-      ..fromEnvelopeProperty.set('Savings')
-      ..toEnvelopeProperty.set('Tithe')
-      ..budgetProperty.set(budgetEntity.id!)
-      ..transactionDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 2)))),
-    IncomeTransaction()
-      ..centsByEnvelopeIdProperty.set({
-        'Tithe': 30 * 100,
-        'Car': 70 * 100,
-        'Emergency Savings': 200 * 100,
-        'Savings': 300 * 100,
-      })
-      ..budgetProperty.set(budgetEntity.id!)
-      ..transactionDateProperty.set(Timestamp.of(DateTime.now().subtract(Duration(days: 1)))),
-  ];
-
-  final budgetChange = budgetEntity.value.addTransactions(
-    dropComponent,
-    envelopeById: envelopes.mapToMap((envelope) => MapEntry(envelope.nameProperty.value, envelope)),
-    transactions: transactions,
-  );
-
-  await Future.wait(budgetChange.modifiedEnvelopeById.mapToIterable((envelopeId, envelope) async {
-    final envelopeEntity = await Query.getById<EnvelopeEntity>(envelopeId).get(dropComponent);
-    await dropComponent.updateEntity(envelopeEntity..set(envelope));
-  }));
-
-  await Future.wait(transactions
-      .map((transaction) =>
-          BudgetTransactionEntity.constructEntityFromTransactionTypeRuntime(transaction.runtimeType)..set(transaction))
-      .map((entity) => dropComponent.update(entity)));
 }
