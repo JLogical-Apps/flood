1a1b52f96407aec713ca7a77576b823c7bd9eeea
jaboyc <jakeboychenko@yahoo.com>
2023-11-06 18:43:24 -0500

    Fix Appwrite serialization not working.

--- packages/utils/utils_core/lib/src/coerce/coerce.dart
+++ packages/utils/utils_core/lib/src/coerce/coerce.dart
@@ -1,15 +1,30 @@
 import 'package:utils_core/src/type/type_utils.dart';
 
 T coerce<T>(dynamic value) {
-  if ((T == int || T == typeOf<int?>()) && value is String) {
-    return int.parse(value) as T;
+  if (value is String) {
+    if (T == int || T == typeOf<int?>()) {
+      return int.parse(value) as T;
+    }
+    if (T == double || T == typeOf<double?>()) {
+      return double.parse(value) as T;
+    }
+    if (T == bool || T == typeOf<bool?>()) {
+      return bool.parse(value) as T;
+    }
   }
-  if ((T == double || T == typeOf<double?>()) && value is String) {
-    return double.parse(value) as T;
+
+  if (value is int) {
+    if (T == double || T == typeOf<double?>()) {
+      return value.roundToDouble() as T;
+    }
   }
-  if ((T == bool || T == typeOf<bool?>()) && value is String) {
-    return bool.parse(value) as T;
+
+  if (value is double) {
+    if (T == int || T == typeOf<int?>()) {
+      return value.floor() as T;
+    }
   }
+
   if (T == String || T == typeOf<String?>()) {
     return value.toString() as T;
   }
@@ -18,14 +33,28 @@ T coerce<T>(dynamic value) {
 }
 
 T? coerceOrNull<T>(dynamic value) {
-  if ((T == int || T == typeOf<int?>()) && value is String) {
-    return int.parse(value) as T;
+  if (value is String) {
+    if (T == int || T == typeOf<int?>()) {
+      return int.parse(value) as T;
+    }
+    if (T == double || T == typeOf<double?>()) {
+      return double.parse(value) as T;
+    }
+    if (T == bool || T == typeOf<bool?>()) {
+      return bool.parse(value) as T;
+    }
   }
-  if ((T == double || T == typeOf<double?>()) && value is String) {
-    return double.parse(value) as T;
+
+  if (value is int) {
+    if (T == double || T == typeOf<double?>()) {
+      return value.roundToDouble() as T;
+    }
   }
-  if ((T == bool || T == typeOf<bool?>()) && value is String) {
-    return bool.parse(value) as T;
+
+  if (value is double) {
+    if (T == int || T == typeOf<int?>()) {
+      return value.floor() as T;
+    }
   }
 
   if (T == String) {
