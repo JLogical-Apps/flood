bf2e3a81c29e64906d091ac89e0e008daad2f00d
jaboyc <jakeboychenko@yahoo.com>
2023-11-17 10:28:19 -0500

    Add onChanged in usePort.

--- packages/port/port/lib/src/port_hooks.dart
+++ packages/port/port/lib/src/port_hooks.dart
@@ -2,8 +2,9 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:port_core/port_core.dart';
 import 'package:utils/utils.dart';
 
-Port<T> usePort<T>(Port<T> Function() portGetter) {
+Port<T> usePort<T>(Port<T> Function() portGetter, {Function()? onChanged}) {
   final port = useMemoized(portGetter);
   useValueStream(port.getPortX());
+  useListen(port.getPortX(), (_) => onChanged?.call());
   return port;
 }
