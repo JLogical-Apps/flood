923b278cacc747ccc993fcbd07280c7cc83d1734
jaboyc <jake@jlogical.com>
2024-04-05 14:35:05 -0400

    Add customization to StyledMarkdown.

--- packages/style/lib/src/components/text/styled_markdown.dart
+++ packages/style/lib/src/components/text/styled_markdown.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:style/src/components/text/styled_text.dart';
+import 'package:style/src/components/text/styled_text_builder.dart';
 import 'package:style/src/style_build_context_extensions.dart';
 import 'package:utils/utils.dart';
 
@@ -8,8 +9,18 @@ class StyledMarkdown extends StatelessWidget {
   final String markdown;
   final WrapAlignment? textAlign;
   final void Function(String href)? onLinkTapped;
+  final StyledTextBuilder baseTextBuilder;
+  final StyledTextBuilder Function(StyledTextBuilder text) textModifier;
 
-  StyledMarkdown(this.markdown, {super.key, this.textAlign, this.onLinkTapped});
+  StyledMarkdown(
+    this.markdown, {
+    super.key,
+    this.textAlign,
+    this.onLinkTapped,
+    StyledTextBuilder? baseTextBuilder,
+    StyledTextBuilder Function(StyledTextBuilder text)? textModifier,
+  })  : baseTextBuilder = baseTextBuilder ?? StyledText.body,
+        textModifier = textModifier ?? _defaultTextModifier;
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +29,18 @@ class StyledMarkdown extends StatelessWidget {
       data: markdown,
       styleSheet: MarkdownStyleSheet(
         textAlign: textAlign ?? WrapAlignment.start,
-        p: style.getTextStyle(context, StyledText.body.empty),
-        h1: style.getTextStyle(context, StyledText.fourXl.empty),
-        h2: style.getTextStyle(context, StyledText.twoXl.empty),
-        h3: style.getTextStyle(context, StyledText.xl.empty),
-        h4: style.getTextStyle(context, StyledText.body.empty),
-        h5: style.getTextStyle(context, StyledText.sm.empty),
-        h6: style.getTextStyle(context, StyledText.xs.empty),
-        code: style.getTextStyle(context, StyledText.body.strong.empty).copyWith(backgroundColor: Colors.transparent),
+        p: style.getTextStyle(context, textModifier(baseTextBuilder).empty),
+        h1: style.getTextStyle(context, textModifier(StyledText.fourXl).empty),
+        h2: style.getTextStyle(context, textModifier(StyledText.twoXl).empty),
+        h3: style.getTextStyle(context, textModifier(StyledText.xl).empty),
+        h4: style.getTextStyle(context, textModifier(StyledText.body).empty),
+        h5: style.getTextStyle(context, textModifier(StyledText.sm).empty),
+        h6: style.getTextStyle(context, textModifier(StyledText.xs).empty),
+        code: style
+            .getTextStyle(context, textModifier(baseTextBuilder).strong.empty)
+            .copyWith(backgroundColor: Colors.transparent),
         a: style
-            .getTextStyle(context, StyledText.body.strong.empty)
+            .getTextStyle(context, textModifier(baseTextBuilder).strong.underline.empty)
             .copyWith(decoration: TextDecoration.underline, decorationColor: context.colorPalette().foreground.strong),
       ),
       softLineBreak: true,
@@ -39,4 +52,8 @@ class StyledMarkdown extends StatelessWidget {
       },
     );
   }
+}
+
+StyledTextBuilder _defaultTextModifier(StyledTextBuilder text) {
+  return text;
 }
