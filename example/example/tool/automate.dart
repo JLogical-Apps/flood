2eb7da6259cf271b1d2f06abc344c6758b745de1
jaboyc <jake@jlogical.com>
2024-04-18 13:43:23 -0400

    Fix imports

--- example/example/tool/automate.dart
+++ example/example/tool/automate.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:jlogical_utils/jlogical_utils_cli.dart';
 import 'package:jlogical_utils/jlogical_utils_core.dart';
 
@@ -13,30 +11,5 @@ Future<void> main(List<String> args) async {
 AutomationContext getAutomationContext() {
   return DefaultAutomationContext()
     ..registerModule(EnvironmentAutomationModule())
-    ..registerModule(AppIconAutomationModule(
-        appIconForegroundFile: Directory.current / 'assets' - 'logo_foreground.png',
-        backgroundColor: 0x172434,
-        banners: [
-          AppIconBanner(
-            environment: Environment.alpha,
-            color: 0xff0000,
-          ),
-          AppIconBanner(
-            environment: Environment.beta,
-            color: 0x9A3CCC,
-          ),
-          AppIconBanner(
-            environment: Environment.uat,
-            color: 0x0000ff,
-          ),
-          AppIconBanner(
-            environment: Environment.qa,
-            color: 0x419C9B,
-          ),
-        ]))
-    ..registerModule(NativeSplashAutomationModule(
-      imageFile: Directory.current / 'assets' - 'logo_foreground.png',
-      backgroundColor: 0x172434,
-    ))
     ..registerModule(FirebaseAutomationModule());
 }
