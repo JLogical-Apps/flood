832b7844c580007649655a0edce85fcd1252f38b
jaboyc <jake@jlogical.com>
2024-09-11 13:54:51 -0400

    Refactor StyleSwitcher.

--- example/example/lib/presentation/components/style_switcher.dart
+++ example/example/lib/presentation/components/style_switcher.dart
@@ -1,4 +1,4 @@
-import 'package:example/presentation/utils/theme_mode_extensions.dart';
+import 'package:example/presentation/style.dart';
 import 'package:example_core/features/local_settings/local_settings.dart';
 import 'package:example_core/features/local_settings/local_settings_entity.dart';
 import 'package:example_core/features/local_settings/theme_mode.dart' as theme;
@@ -6,20 +6,12 @@ import 'package:flood/flood.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ThemeSwitcher extends HookWidget {
-  const ThemeSwitcher({super.key});
+class StyleSwitcher extends HookWidget {
+  const StyleSwitcher({super.key});
 
   @override
   Widget build(BuildContext context) {
     final localSettingsModel = useSingleton(Query.from<LocalSettingsEntity>());
-
-    useListen(
-      useMemoized(() => localSettingsModel.stateX
-          .whereLoaded()
-          .map((localSettingsEntity) => localSettingsEntity.value.themeProperty.value)),
-      (theme.ThemeMode themeMode) => context.styleAppComponent.style = themeMode.style,
-    );
-
     return ModelBuilder(
       model: localSettingsModel,
       builder: (LocalSettingsEntity localSettingsEntity) {
@@ -27,18 +19,20 @@ class ThemeSwitcher extends HookWidget {
             ? StyledButton(
                 iconData: Icons.dark_mode,
                 onPressed: () async {
+                  context.styleAppComponent.style = lightStyle;
                   await context.dropCoreComponent.updateEntity(
                     localSettingsEntity,
-                    (LocalSettings user) => user..themeProperty.set(theme.ThemeMode.light),
+                    (LocalSettings localSettings) => localSettings..themeProperty.set(theme.ThemeMode.light),
                   );
                 },
               )
             : StyledButton(
                 iconData: Icons.light_mode,
                 onPressed: () async {
+                  context.styleAppComponent.style = darkStyle;
                   await context.dropCoreComponent.updateEntity(
                     localSettingsEntity,
-                    (LocalSettings user) => user..themeProperty.set(theme.ThemeMode.dark),
+                    (LocalSettings localSettings) => localSettings..themeProperty.set(theme.ThemeMode.dark),
                   );
                 },
               );
